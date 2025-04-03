cd738966bb32568478a1a0502839f40d454863e4
rwaedWADSADA <rwaedWADSADA@proton.me>
2025-04-03 23:19:30 +0100

    Create FpsCap.lua

---
+++ Scripts/FpsCap.lua
@@ -0,0 +1,20 @@
+local fpscap = math.huge
+
+getgenv().setfpscap = function(cap)
+    cap = tonumber(cap)
+    assert(type(cap) == "number", "invalid argument #1 to 'setfpscap' (number expected, got " .. type(cap) .. ")", 2)
+    if cap < 1 then cap = math.huge end
+    fpscap = cap
+end
+
+local clock = tick()
+game:GetService("RunService").RenderStepped:Connect(function()
+    while clock + 1 / fpscap > tick() do end
+    clock = tick()
+
+    task.wait()
+end)
+
+getgenv().getfpscap = function()
+    return fpscap
+end
